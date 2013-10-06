4c829cef39ad0161b6f8a5de463443b4de7c9ffc
Kruglik Alexey <seldoff@gmail.com>
2013-10-06 12:54:58 +0300

    Refactor events module

--- Jeegoordah.Web/Scripts/Jeegoordah/modules/events.js
+++ Jeegoordah.Web/Scripts/Jeegoordah/modules/events.js
@@ -11,11 +11,16 @@
             $('#modules').empty().append($(moduleTemplate));
             $('#createEventButton').click(function () {
                 editor.show($(editorTemplate), {}, 'Create Event', _.bind(self._createEvent, self));
-            });
+            });            
+            this._loadEvents();            
+        },
+        
+        _loadEvents: function () {
+            var self = this;
             $('#event-list>tr').remove();
-            rest.get('events').done(function (events) {                
+            rest.get('events').done(function (events) {
                 // TODO add spinner while loading
-                _.each(events, function (event) {                    
+                _.each(events, function (event) {
                     event = self._fixStartDateFormat(event);
                     self._createEventElement(event);
                 });
@@ -34,7 +39,6 @@
         },
         
         _createEventElement: function (event, appendToList) {
-            var self = this;
             if (_.isUndefined(appendToList)) {
                 appendToList = true;
             }
@@ -46,48 +50,55 @@
                 eventList.append($event);
             }            
 
-            $event.find('button[data-action=edit]').click(function () {
-                editor.show($(editorTemplate), event, 'Edit Event', function(updatedEvent) {
-                    updatedEvent = _.extend({ Id: event.Id }, updatedEvent);
-                    rest.post('events/update', updatedEvent).done(function () {
-                        editor.close();
-                        notification.success('Event updated.');
+            $event.find('button[data-action=edit]').click(_.bind(this._editEvent, this, event));
+            $event.find('button[data-action=delete]').click(_.bind(this._deleteEvent, this, event));
+            return $event;
+        },
+        
+        _editEvent: function (event) {
+            var self = this;
+            editor.show($(editorTemplate), event, 'Edit Event', function (updatedEvent) {
+                updatedEvent = _.extend({ Id: event.Id }, updatedEvent);
+                rest.post('events/update', updatedEvent).done(function () {
+                    editor.close();
+                    notification.success('Event updated.');
 
-                        var oldElement = eventList.find('#event' + event.Id);
-                        var newElement = self._createEventElement(updatedEvent, false);
-                        oldElement.fadeOut(consts.fadeDuration, function () {
-                            newElement.insertAfter(oldElement).hide();
-                            oldElement.remove();
-                            newElement.fadeIn(consts.fadeDuration);
-                        });
+                    var oldElement = $('#event-list').find('#event' + event.Id);
+                    var newElement = self._createEventElement(updatedEvent, false);
+                    oldElement.fadeOut(consts.fadeDuration, function () {
+                        newElement.insertAfter(oldElement).hide();
+                        oldElement.remove();
+                        newElement.fadeIn(consts.fadeDuration);
                     });
                 });
+            });
+        },
+        
+        _deleteEvent: function(event) {
+            var $event = $('#event-list>#event' + event.Id);
+            var $popoverTarget = $event.find('button[data-action=delete]');
+            var $content = $('<div style="min-width: 100px"><div class="btn btn-danger" data-action="Yes">Yes</div>' +
+                             '<div class="btn btn-default" data-action="No">No</div></div>');
+            
+            $content.find('[data-action=Yes]').click(function () {
+                rest.post('events/delete/' + event.Id).done(function () {
+                    $event.fadeOut(consts.fadeDuration, function () {
+                        $event.remove();
+                    });
+                });
+                $popoverTarget.popover('destroy');
             });
             
-            $event.find('button[data-action=delete]').click(function () {
-                var popoverTarget = this;
-                var $content = $('<div style="min-width: 100px"><div class="btn btn-danger" data-action="Yes">Yes</div>' +
-                                 '<div class="btn btn-default" data-action="No">No</div></div>');
-                $content.find('[data-action=Yes]').click(function() {
-                    rest.post('events/delete/' + event.Id).done(function () {                        
-                        $event.fadeOut(consts.fadeDuration, function() {
-                            $event.remove();
-                        });
-                    });
-                    $(popoverTarget).popover('destroy');
-                });
-                $content.find('[data-action=No]').click(function () {
-                    $(popoverTarget).popover('destroy');
-                });
-                $(popoverTarget).popover({
-                    title: 'Are you sure?',
-                    content: $content,
-                    html: true
-                });
-                $(popoverTarget).popover('show');
+            $content.find('[data-action=No]').click(function () {
+                $popoverTarget.popover('destroy');
             });
-
-            return $event;
+            
+            $popoverTarget.popover({
+                title: 'Are you sure?',
+                content: $content,
+                html: true
+            });
+            $popoverTarget.popover('show');
         },
         
         _fixStartDateFormat: function (event) {
