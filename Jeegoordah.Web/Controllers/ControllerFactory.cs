509bcaca052bf1fd9cf734f5ef468800af1fead1
Kruglik Alexey <seldoff@gmail.com>
2013-10-09 00:06:42 +0300

    Fix exception in Release mode

--- Jeegoordah.Web/Controllers/ControllerFactory.cs
+++ Jeegoordah.Web/Controllers/ControllerFactory.cs
@@ -14,15 +14,19 @@ namespace Jeegoordah.Web.Controllers
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
             var controller = Activator.CreateInstance(controllerType);
-#if DEBUG
+
             if (_dbControllerType.IsAssignableFrom(controllerType))
             {
+#if DEBUG
                 var dbFactory = requestContext.HttpContext.Request.Params["test"] == null
                    ? new DbFactory(typeof(JeegoordahRealDb))
                    : new DbFactory(typeof(JeegoordahTestDb));
                 ((DbController)controller).SetDbFactory(dbFactory);   
-            }            
+#else
+                ((DbController)controller).SetDbFactory(new DbFactory(typeof(JeegoordahRealDb))); 
 #endif
+            }            
+
             return (IController)controller;
         }
     }
