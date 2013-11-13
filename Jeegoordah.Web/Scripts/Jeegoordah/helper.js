95168febffa26eb905adf67015966a1f04e55c42
Kruglik Alexey <seldoff@gmail.com>
2013-11-13 23:40:16 +0300

    Open links from comments in the new tab

--- Jeegoordah.Web/Scripts/Jeegoordah/helper.js
+++ Jeegoordah.Web/Scripts/Jeegoordah/helper.js
@@ -3,7 +3,7 @@
         // Replaces urls with <a> tags. Replaces newlines with <br /> tag.
         textToHtml: function(text) {            
             var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
-            return text.replace(exp, "<a href='$1'>$1</a>").replace('\r\n','<br />');           
+            return text.replace(exp, '<a href="$1" target="_blank">$1</a>').replace('\r\n','<br />');           
         },
         
         // Converts date string 01-10-2013 to the date object
