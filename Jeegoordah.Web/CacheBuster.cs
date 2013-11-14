feb35c01d9a0d7f104075d00eff6b67c33320041
Kruglik Alexey <seldoff@gmail.com>
2013-11-14 22:53:41 +0300

    Increment version

--- Jeegoordah.Web/CacheBuster.cs
+++ Jeegoordah.Web/CacheBuster.cs
@@ -12,7 +12,7 @@ namespace Jeegoordah.Web
 #if DEBUG
             _cacheBuster = (new Random()).Next().ToString();
 #else
-            _cacheBuster = "2.0.1";
+            _cacheBuster = "2.0.2";
 #endif
         }
 
