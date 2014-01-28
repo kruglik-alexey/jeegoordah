5d0f969dc1b62515dadaa13005073ba1c64f3295
Kruglik Alexey <kruglik@targetprocess.com>
2014-01-28 22:04:15 +0300

    Version 2.1.0

--- Jeegoordah.Web/CacheBuster.cs
+++ Jeegoordah.Web/CacheBuster.cs
@@ -12,7 +12,7 @@ namespace Jeegoordah.Web
 #if DEBUG
             _cacheBuster = (new Random()).Next().ToString();
 #else
-            _cacheBuster = "2.0.9";
+            _cacheBuster = "2.1.0";
 #endif
         }
 
