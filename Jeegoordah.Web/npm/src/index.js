59bee521dec6efc0a085c6d1f3674ef71c7365df
Kruglik Alexey <kruglik@targetprocess.com>
2016-06-28 21:51:35 +0300

    Add bootstrap and react

--- Jeegoordah.Web/npm/src/index.js
+++ Jeegoordah.Web/npm/src/index.js
@@ -1,1 +1,10 @@
-import "babel-polyfill";
+import 'babel-polyfill'
+import 'bootstrap-loader'
+import IndexView from './views/index'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import domready from 'domready'
+
+domready(() => {
+    ReactDOM.render(React.createElement(IndexView), document.getElementById('app-container'));
+});
---
+++ Jeegoordah.Web/npm/src/views/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class IndexView extends React.Component {
+    render() {
+        return (<div className="container"><h1>Foo</h1></div>);
+    }
+}
