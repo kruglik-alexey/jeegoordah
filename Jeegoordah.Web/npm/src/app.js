ebeb4b2d4d88f5d5eaa1856452a7f5c51279d6d7
kruglik <seldoff@gmail.com>
2016-07-06 22:24:36 +0300

    Extract store to a separate module

--- Jeegoordah.Web/npm/src/app.js
+++ Jeegoordah.Web/npm/src/app.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import domReady from 'domready'
 import {get} from './rest'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
-import rootReducer from './reducers/root'
 import actions from './actions'
 import * as _ from 'lodash'
+import store from './store'
 
 const $domReady = new Promise(resolve => domReady(resolve));
-
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
 
 store.dispatch(dispatch => {
     const $bros = get('bros');
---
+++ Jeegoordah.Web/npm/src/store.js
@@ -0,0 +1,6 @@
+import rootReducer from './reducers/root'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+export default store;
