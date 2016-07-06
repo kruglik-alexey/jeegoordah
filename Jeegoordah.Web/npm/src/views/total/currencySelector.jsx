fd55b4732e17e4d3fb6e6b240409ab296e86acae
kruglik <seldoff@gmail.com>
2016-07-06 20:18:28 +0300

    Show only some currencies in total

--- Jeegoordah.Web/npm/src/views/total/currencySelector.jsx
+++ Jeegoordah.Web/npm/src/views/total/currencySelector.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import classNames from 'classnames'
+import * as _ from 'lodash'
+
+const visibleCurrencies = ['USD', 'EUR', 'BYR', 'BYN'];
 
 export default function(props) {
-    const currencies = props.currencies.map(c => {
-        const cs = classNames({active: c.id === props.selectedCurrency});
-        return (
-            <li key={c.id} className={cs}>
-                <a onClick={() => props.onSelectCurrency(c.id)}>{c.name}</a>
-            </li>
-        );
-    });
+    const currencies = visibleCurrencies
+        .map(c => _.find(props.currencies, {name: c}))
+        .map(c => {
+            const cs = classNames({active: c.id === props.selectedCurrency});
+            return (
+                <li key={c.id} className={cs}>
+                    <a onClick={() => props.onSelectCurrency(c.id)}>{c.name}</a>
+                </li>
+            );
+        });
 
     return (
         <ul className="nav nav-pills">
