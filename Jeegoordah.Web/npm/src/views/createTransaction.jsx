4ddd679aa654fd23e2ee0e05e145f95fab57ef57
kruglik <seldoff@gmail.com>
2016-07-22 20:17:16 +0300

    Transaction editor WIP

--- Jeegoordah.Web/npm/src/views/createTransaction.jsx
+++ Jeegoordah.Web/npm/src/views/createTransaction.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './header'
 import $ from 'jquery'
+import {connect} from 'react-redux'
 
-export default class extends React.Component {
+class CreateTransactionView extends React.Component {
     componentDidMount() {
         $(this.refs.dateInput).datepicker({
             format: 'dd-mm-yyyy',
-            autoclose: true
+            autoclose: true,
+            weekStart: 1
         });
     }
 
@@ -21,10 +23,35 @@ export default class extends React.Component {
                 <form>
                     <div className="form-group">
                         <label>Date</label>
-                        <input className="form-control" type="text" name="date" ref="dateInput" value="01-01-2000"/>
+                        <input className="form-control" type="text" name="date" ref="dateInput"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Amount</label>
+                        <div className="row">
+                            <div className="col-sm-12">
+                                <div className="input-group">
+                                    <input className="form-control" type="text" name="amount"/>
+                                    <span className="input-group-btn">
+                                        {this.renderCurrencies(this.props.currencies)}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
         );
     }
+
+    renderCurrencies(currencies) {
+        return currencies.map(c => <label key={c.id}>{c.name}</label>);
+    }
 }
+
+const stateToProps = state => {
+    return {
+        currencies: state.data.currencies
+    }
+};
+
+export default connect(stateToProps)(CreateTransactionView)
